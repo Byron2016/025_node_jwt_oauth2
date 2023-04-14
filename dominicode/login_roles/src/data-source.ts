2cf8d9ce52d1e8222ec1e0d2b1f3982b1f96905b
Byron <bgva2005@yahoo.com>
2023-04-14 11:39:37 -0500

    feat: add data-source file to be able to read from enviroment variables

--- dominicode/login_roles/src/data-source.ts
+++ dominicode/login_roles/src/data-source.ts
@@ -1,15 +1,15 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
-const dataKey:any = require("../data-key.json");
+const dataKey:any = require("./data-key.json");
 
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "aws.connect.psdb.cloud",
     port: 3306,
-    username: dataKey.database.username,
-    password: dataKey.database.password,
-    database: "login_node",
+    username: process.env.USERNAMEDC || dataKey.database.username,
+    password: process.env.PASSWORDDC || dataKey.database.password,
+    database: process.env.DATABASEDC || dataKey.database.database,
     synchronize: true,
     logging: false,
     entities: [User],
