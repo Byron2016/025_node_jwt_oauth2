c81c0edd48b993690bf02cd7d16eed95c1926d8f
Byron <bgva2005@yahoo.com>
2023-04-19 12:26:37 -0500

    feat: vercel.json update

--- dominicode/login_roles/src/index.ts
+++ dominicode/login_roles/src/index.ts
@@ -1,31 +1,32 @@
-import * as express from "express"
-import { Request, Response } from "express"
-import { AppDataSource } from "./data-source"
-import * as cors from 'cors'
+import * as express from "express";
+import { Request, Response } from "express";
+import { AppDataSource } from "./data-source";
+import * as cors from "cors";
 import helmet from "helmet"; // Security
-import routes from "./routes"
+import routes from "./routes";
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-AppDataSource.initialize().then(async () => {
-
+AppDataSource.initialize()
+  .then(async () => {
     // create express app
-    const app = express()
+    const app = express();
     // Middlewares
-    app.use(cors())
-    app.use(helmet())
+    app.use(cors());
+    app.use(helmet());
 
-
-    app.use(express.json())
+    app.use(express.json());
     // Routes
-    app.use('/', routes)
+    app.use("/", routes);
 
     // start express server
     app.listen(PORT, () => {
-        console.log(`Express server has started on port ${PORT}. Open http://localhost:${PORT}/users to see results`)
-    })
-
-}).catch(error => console.log(error))
+      console.log(
+        `Express server has started on port ${PORT}. Open http://localhost:${PORT}/users to see results`
+      );
+    });
+  })
+  .catch((error) => console.log(error));
 
 // let count = 0;
 // setInterval(() => {
--- dominicode/login_roles/src/routes/test.ts
+++ dominicode/login_roles/src/routes/test.ts
@@ -1,18 +1,30 @@
-import { Router } from 'express'
-import { Request, Response } from "express"
+import { Router } from "express";
+import { Request, Response } from "express";
 
-const router = Router()
+const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
-    try {
-        res.json({
-            status:200,
-            message: "Get data has sucessfully",
-        });
-    } catch (e) {
-        console.log(e);
-        return res.status(500).send("Server Error (test.js)")
-    }
-})
+  try {
+    res.json({
+      status: 200,
+      message: "025_NODE_JWT_OAUTH_test-page 🐸🐸🐸",
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send("Server Error (test.js)");
+  }
+});
 
-export default router
+router.get("/db", async (req: Request, res: Response) => {
+  try {
+    res.json({
+      status: 200,
+      message: "Get data has sucessfully",
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send("Server Error (test.js)");
+  }
+});
+
+export default router;
